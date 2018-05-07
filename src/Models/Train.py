ace2126a23efae8827af72b865af81c361daf55d
Maxim Orel <maximorel@Maxims-MacBook-Pro.local>
2018-05-07 11:06:07 +0300

    build Train model for btc and eth

--- src/Models/Train.py
+++ src/Models/Train.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import datetime
 import sys
-import io
 import urllib
 import keras
 
@@ -19,78 +18,86 @@ class Train(object):
     @staticmethod
     def trainCoin(coin):
         model_path = ('assets/%s_model.h5' % coin)
+        window_len = 20
+        epochs = 100
+        neurons_lv1 = 50
+        neurons_lv2 = 25
+        batch_size = 32
 
         if coin == 'btc':
             start_date = '20131227'
-            end_date = '20180425'
-            split_date = '2018-02-25'
-            window_len = 20
-            epochs = 100
-            batch_size = 32
-            neurons_lv1 = 50
-            neurons_lv2 = 25
+            end_date = '20180501'
+            split_date = '2018-03-01'
             activation_func = 'linear'
             dropout = 0.5
             loss = 'mean_squared_error'
             optimizer = 'adam'
+            data_url = 'https://coinmarketcap.com/currencies/bitcoin/historical-data/?start='
+        elif coin == 'eth':
+            start_date = '20150808'
+            end_date = '20180501'
+            split_date = '2018-03-01'
+            data_url = 'https://coinmarketcap.com/currencies/ethereum/historical-data/?start='
 
-            btc_data = pd.read_html("https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=" + start_date + "&end=" + end_date)[0]
-            btc_data = btc_data.assign(Date=pd.to_datetime(btc_data['Date']))
+        crypto_data = pd.read_html(data_url + start_date + "&end=" + end_date)[0]
+        crypto_data = crypto_data.assign(Date=pd.to_datetime(crypto_data['Date']))
 
-            print(btc_data.head())
-            print('Shape: {}'.format(btc_data.shape))
-            Plot.drawTrend(btc_data, "Bitcoin")
-            print('\n')
+        print(crypto_data.head())
+        print('Shape: {}'.format(crypto_data.shape))
+        Plot.drawTrend(crypto_data, coin.upper())
+        print('\n')
 
-            btc_data.columns = [btc_data.columns[0]] + [coin + '_' + i for i in btc_data.columns[1:]]
-            kwargs = {coin + '_day_diff': lambda x: (x[coin + '_Close'] - x[coin + '_Open']) / x[coin + '_Open']}
-            btc_data = btc_data.assign(**kwargs)
+        crypto_data.columns = [crypto_data.columns[0]] + [coin + '_' + i for i in crypto_data.columns[1:]]
+        kwargs = {coin + '_day_diff': lambda x: (x[coin + '_Close'] - x[coin + '_Open']) / x[coin + '_Open']}
+        crypto_data = crypto_data.assign(**kwargs)
 
-            print(btc_data.head())
-            print('Shape: {}'.format(btc_data.shape))
-            print('\n')
-            Plot.drawTest(btc_data, split_date, "btc_Close", "Bitcoin")
+        print(crypto_data.head())
+        print('Shape: {}'.format(crypto_data.shape))
+        print('\n')
+        Plot.drawTest(crypto_data, split_date, coin + "_Close", coin.upper())
 
-            kwargs = {coin + '_close_off_high': lambda x: 2 * (x[coin + '_High'] - x[coin + '_Close']) / (x[coin + '_High'] - x[coin + '_Low']) -1,
-                      coin + '_volatility': lambda x: (x[coin + '_High'] - x[coin + '_Low']) / (x[coin + '_Open'])}
-            btc_data = btc_data.assign(**kwargs)
-            model_data = btc_data[['Date'] + [coin + metric for metric in ['_Close', '_Volume', '_close_off_high', '_volatility', '_day_diff', '_Market Cap']]]
-            model_data = model_data.sort_values(by='Date')
+        kwargs = {coin + '_close_off_high': lambda x: 2 * (x[coin + '_High'] - x[coin + '_Close']) / (x[coin + '_High'] - x[coin + '_Low']) -1,
+                  coin + '_volatility': lambda x: (x[coin + '_High'] - x[coin + '_Low']) / (x[coin + '_Open'])}
+        crypto_data = crypto_data.assign(**kwargs)
+        model_data = crypto_data[['Date'] + [coin + metric for metric in ['_Close', '_Volume', '_close_off_high', '_volatility', '_day_diff', '_Market Cap']]]
+        model_data = model_data.sort_values(by='Date')
 
-            print(model_data.head())
-            print('Shape: {}'.format(model_data.shape))
-            print("\n")
+        print(model_data.head())
+        print('Shape: {}'.format(model_data.shape))
+        print("\n")
 
-            training_set, test_set = model_data[model_data['Date'] < split_date], model_data[model_data['Date'] >= split_date]
-            training_set = training_set.drop('Date', 1)
-            test_set =  test_set.drop('Date', 1)
-            normalise_cols = [coin + metric for metric in ['_Close', '_Volume', '_Market Cap']]
+        training_set, test_set = model_data[model_data['Date'] < split_date], model_data[model_data['Date'] >= split_date]
+        training_set = training_set.drop('Date', 1)
+        test_set =  test_set.drop('Date', 1)
+        normalise_cols = [coin + metric for metric in ['_Close', '_Volume', '_Market Cap']]
 
-            LSTM_training_inputs = NeuralNetwork.normaliseInput(training_set, normalise_cols, window_len)
-            LSTM_training_outputs = NeuralNetwork.normaliseOutput(training_set, coin+'_Close', window_len)
-            LSTM_test_inputs = NeuralNetwork.normaliseInput(test_set, normalise_cols, window_len)
-            LSTM_test_outputs = NeuralNetwork.normaliseOutput(test_set, coin+'_Close', window_len)
+        LSTM_training_inputs = NeuralNetwork.normaliseInput(training_set, normalise_cols, window_len)
+        LSTM_training_outputs = NeuralNetwork.normaliseOutput(training_set, coin+'_Close', window_len)
+        LSTM_test_inputs = NeuralNetwork.normaliseInput(test_set, normalise_cols, window_len)
+        LSTM_test_outputs = NeuralNetwork.normaliseOutput(test_set, coin+'_Close', window_len)
 
-            print("\nNumber Of Input Training's sequences: {}".format(len(LSTM_training_inputs)))
-            print("\nNumber Of Output Training's sequences: {}".format(len(LSTM_training_outputs)))
-            print("\nNumber Of Input Test's sequences: {}".format(len(LSTM_test_inputs)))
-            print("\nNumber Of Output Test's sequences: {}".format(len(LSTM_test_outputs)))
+        print("\nNumber Of Input Training's sequences: {}".format(len(LSTM_training_inputs)))
+        print("\nNumber Of Output Training's sequences: {}".format(len(LSTM_training_outputs)))
+        print("\nNumber Of Input Test's sequences: {}".format(len(LSTM_test_inputs)))
+        print("\nNumber Of Output Test's sequences: {}".format(len(LSTM_test_outputs)))
 
-            LSTM_training_inputs = [np.array(LSTM_training_input) for LSTM_training_input in LSTM_training_inputs]
-            LSTM_training_inputs = np.array(LSTM_training_inputs)
-            LSTM_test_inputs = [np.array(LSTM_test_inputs) for LSTM_test_inputs in LSTM_test_inputs]
-            LSTM_test_inputs = np.array(LSTM_test_inputs)
+        LSTM_training_inputs = [np.array(LSTM_training_input) for LSTM_training_input in LSTM_training_inputs]
+        LSTM_training_inputs = np.array(LSTM_training_inputs)
+        LSTM_test_inputs = [np.array(LSTM_test_inputs) for LSTM_test_inputs in LSTM_test_inputs]
+        LSTM_test_inputs = np.array(LSTM_test_inputs)
 
-            model = NeuralNetwork.buildModel(LSTM_training_inputs, output_size=1, neurons_lv1=neurons_lv1,
-                                             neurons_lv2=neurons_lv2, activation_func=activation_func,
-                                             dropout=dropout, loss=loss, optimizer=optimizer)
-            train_model =  model.fit(LSTM_training_inputs, LSTM_training_outputs, epochs=epochs,
-                                     batch_size=batch_size, verbose=2, shuffle=True, validation_split=0.2,
-                                     callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, mode='min'),
-                                                keras.callbacks.ModelCheckpoint(model_path, monitor='val_loss', save_best_only=True, mode='min', verbose=0)])
-            scores = model.evaluate(LSTM_test_inputs, LSTM_test_outputs, verbose=1, batch_size=batch_size)
-            print('\nMSE: {}'.format(scores[1]))
-            print('\nMAE: {}'.format(scores[2]))
-            print('\nR^2: {}'.format(scores[3]))
+        model = NeuralNetwork.buildModel(LSTM_training_inputs, output_size=1, neurons_lv1=neurons_lv1, neurons_lv2=neurons_lv2)
+        train_model =  model.fit(LSTM_training_inputs, LSTM_training_outputs, epochs=epochs,
+                                 batch_size=batch_size, verbose=2, shuffle=True, validation_split=0.2,
+                                 callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, mode='min'),
+                                            keras.callbacks.ModelCheckpoint(model_path, monitor='val_loss', save_best_only=True, mode='min', verbose=0)])
 
-            return True
+        scores = model.evaluate(LSTM_test_inputs, LSTM_test_outputs, verbose=1, batch_size=batch_size)
+        print('\nMSE: {}'.format(scores[1]))
+        print('MAE: {}'.format(scores[2]))
+        print('R^2: {}\n'.format(scores[3]))
+
+        Plot.drawError(model, train_model, coin.upper())
+        Plot.drawDiff(model, model_data, test_set, LSTM_test_inputs, split_date, window_len, coin)
+
+        return True
--- src/Utils/Plot.py
+++ src/Utils/Plot.py
@@ -1,5 +1,6 @@
 import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Plot(object):
@@ -43,3 +44,36 @@ class Plot(object):
         plt.tight_layout()
         plt.show()
         fig.savefig("assets/" + coin + "Test.png")
+
+    @staticmethod
+    def drawError(model, train_model, coin):
+        fig_err, ax = plt.subplots(1, 1)
+        ax.plot(train_model.epoch, train_model.history['loss'])
+        ax.set_title('Training Error')
+        if model.loss == 'mae':
+            ax.set_ylabel('Mean Absolute Error (MAE)', fontsize=12)
+        else:
+            ax.set_ylabel('Model Loss', fontsize=12)
+        ax.set_xlabel('Epochs', fontsize=12)
+        plt.show()
+        fig_err.savefig("assets/" + coin + "Error")
+
+    @staticmethod
+    def drawDiff(model, model_data, test_set, LSTM_test_inputs, split_date, window_len, coin):
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        ax.set_xticks([datetime.date(2018, i + 1, 1) for i in range(6)])
+        ax.set_xticklabels([datetime.date(2018, i+1, 1).strftime('%b %d %Y') for i in range(6)])
+        ax.plot(model_data[model_data['Date'] >= split_date]['Date'][window_len:].astype(datetime.datetime),
+                test_set[coin + '_Close'][window_len:], label='Actual')
+        ax.plot(model_data[model_data['Date'] >= split_date]['Date'][window_len:].astype(datetime.datetime),
+                ((np.transpose(model.predict(LSTM_test_inputs)) + 1) * test_set[coin + '_Close'].values[:-window_len])[0],
+                label='Predicted')
+        ax.annotate('MAE: %.4f' % np.mean(np.abs((np.transpose(model.predict(LSTM_test_inputs)) + 1) - \
+                                                 (test_set[coin + '_Close'].values[window_len:]) / (test_set[coin + '_Close'].values[:-window_len]))),
+                                                 xy=(0.75, 0.9), xycoords='axes fraction',
+                                                 xytext=(0.75, 0.9), textcoords='axes fraction')
+        ax.set_title('Test Set: Single Timepoint Prediction', fontsize=13)
+        ax.set_ylabel(coin.upper() + ' Price ($)', fontsize=12)
+        ax.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 14})
+        plt.show()
+        fig.savefig("assets/" + coin.upper() + "Diff.png")
